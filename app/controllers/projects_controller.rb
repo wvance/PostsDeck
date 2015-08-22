7d8bc7b5ed26e78c8316c34d6d88155cceb9fc49
Wesley Vance <wesley.vance@mac.com>
2015-08-21 20:50:42 -0400

    Added admin bars

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,7 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!
+  
   # GET /projects
   # GET /projects.json
   def index
