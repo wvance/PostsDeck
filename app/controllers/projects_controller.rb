72c1b451c75efa9a13da2a3aeb773d6417355409
Wesley Vance <wesley.vance@mac.com>
2015-08-25 14:13:05 -0400

    Deleted

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -68,7 +68,6 @@ class ProjectsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to request.referrer, notice: 'Project was successfully destroyed.' }
       format.json { head :no_content }
-      # format.js   { render :layout => false }
     end
   end
 
