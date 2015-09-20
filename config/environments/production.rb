6e67dc17ee6d08f5ad81362ab77d141b5d987cee
Wesley Vance <wesley.vance@mac.com>
2015-09-20 17:24:17 -0500

    Fix for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_dispatch.tld_length = 2
   # Code is not reloaded between requests.
   config.cache_classes = true
 
