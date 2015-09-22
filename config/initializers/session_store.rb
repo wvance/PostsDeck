de04016ab8a42380ab020715a319c48229d45e39
Wesley Vance <wesley.vance@mac.com>
2015-09-21 23:34:03 -0500

    Fixed

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-WesleyVance::Application.config.session_store :cookie_store, :key => '_WesleyVance_session', :domain => :all
-# Rails.application.config.session_store :cookie_store, key: '_WesleyVance_session', :domain => 'lvh.me', :tld_length => 2
+Rails.application.config.session_store :cookie_store, key: '_WesleyVance_session', :domain => '.blackboxapp.io'
