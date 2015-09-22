c3d396251c0d98b0343b2170d3dc1e3d58114ed4
Wesley Vance <wesley.vance@mac.com>
2015-09-21 23:11:36 -0500

    Updated

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_WesleyVance_session', :domain => 'lvh.me', :tld_length => 2
+WesleyVance::Application.config.session_store :cookie_store, :key => '_WesleyVance_session', :domain => :all
+# Rails.application.config.session_store :cookie_store, key: '_WesleyVance_session', :domain => 'lvh.me', :tld_length => 2
