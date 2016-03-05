29dbeb6b73ab051c03b7a59dd7956e0b1b7847ec
Wesley Vance <wesley.vance@mac.com>
2016-03-05 12:49:51 -0600

    Update full width project if picture is not present

--- app/controllers/emailapi_controller.rb
+++ app/controllers/emailapi_controller.rb
@@ -1,25 +1,14 @@
 class EmailapiController < ApplicationController
-
-  def index
-  end
-
   def subscribe
     @list_id = ENV["mail_chimp_list_id"]
-    gb = Gibbon::Request.new
+    gibbon = Gibbon::Request.new
 
-    # gb.lists.subscribe({
-    #   :id => @list_id,
-    #   :email => {:email => params[:email][:address]}
-    # })
-    # gibbon.lists.subscribe({:id => list_id, :email => {:email => "foo@bar.com"}, :merge_vars => {:FNAME => "Bob", :LNAME => "Smith"}})
-
-    gb.lists(@list_id).members.create(
+    gibbon.lists(@list_id).members.create(
       body: {
         email_address: params[:email][:address],
         status: "subscribed"
       }
     )
-    # gibbon.lists(list_id).members.create(body: {email_address: "foo@bar.com", status: "subscribed", merge_fields: {FNAME: "Bob", LNAME: "Smith"}})
 
     respond_to do |format|
       format.json{render :json => {:message => "You have been Successfully added to the list! :)"}}
