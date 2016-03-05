5368752224f1025eaaa7835edf471536892608fc
Wesley Vance <wesley.vance@mac.com>
2016-03-05 17:29:29 -0600

    Updated code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,4 @@
 module ApplicationHelper
-
-  class HTML < Redcarpet::Render::HTML
-    # to use Rouge with Redcarpet
-    include Rouge::Plugins::Redcarpet
-    # overriding Redcarpet method
-    # github.com/vmg/redcarpet/blob/master/lib/redcarpet/render_man.rb#L9
-    def block_code(code, language)
-      # highlight some code with a given language lexer
-      # and formatter: html or terminal256
-      # and block if you want to stream chunks
-      # github.com/jayferd/rouge/blob/master/lib/rouge.rb#L17
-      Rouge.highlight(code, language || 'text', 'html')
-      # watch out you need to provide 'text' as a default,
-      # because when you not provide language in Markdown
-      # you will get error: <RuntimeError: unknown lexer >
-    end
-  end
-
   def title(page_title)
     content_for :title, page_title.to_s
   end
--- config/initializers/rouge.rb
+++ config/initializers/rouge.rb
@@ -1,1 +1,20 @@
 require 'rouge/plugins/redcarpet'
+
+class HTML < Redcarpet::Render::HTML
+  # to use Rouge with Redcarpet
+  include Rouge::Plugins::Redcarpet
+
+  def rouge_formatter(opts={})
+    opts ={
+      line_numbers: true,
+      wrap: true,
+    }
+    Rouge::Formatters::HTML.new(opts)
+  end
+
+  # overriding Redcarpet method
+  # github.com/vmg/redcarpet/blob/master/lib/redcarpet/render_man.rb#L9
+  def block_code(code, language)
+    Rouge.highlight(code, language || 'text', 'html')
+  end
+end
