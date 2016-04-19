08e59fa62c1d3894a9762c0bb0dbc59c4fdf1f7d
Wesley Vance <wesley.vance@mac.com>
2016-04-19 00:53:43 -0500

    updated testimonial styles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -107,7 +107,7 @@ class UsersController < ApplicationController
 		respond_to do |format|
 		  format.html
 		  format.json { render json: @geojson }  # respond with the created JSON object
-		  # format.js
+		  format.js
 		end
 	end
 
