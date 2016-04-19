145d653c0ee54362c5870adeb2d116d82bab885d
Wesley Vance <wesley.vance@mac.com>
2016-04-18 22:47:40 -0500

    Updated testimonical view when empty

--- db/migrate/20160415180212_add_related_content_to_contents.rb
+++ db/migrate/20160415180212_add_related_content_to_contents.rb
@@ -1,5 +1,5 @@
 class AddRelatedContentToContents < ActiveRecord::Migration[5.0]
   def change
-    # add_column :contents, :related_contents, :string
+    add_column :contents, :related_contents, :string
   end
 end
